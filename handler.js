06b6d779b9e02a6b9d8ec812744849264267e6a3
MrElvin1 <935448078@qq.com>
2018-04-30 11:32:45 +0800

    show ok

--- handler.js
+++ handler.js
@@ -4,13 +4,15 @@ const ProgressBar = require('progress')
 const chalk = require('chalk')
 const fs = require('fs')
 const path = require('path')
+const util = require('util')
 
 const DEFAULT_COUNT = 50
 
 const URL = {
   IMDB: `http://api.douban.com/v2/movie/top250`,
   SHOW_THEATERS: `http://api.douban.com/v2/movie/in_theaters?count=${DEFAULT_COUNT}`,
-  COMING_SOON: `http://api.douban.com/v2/movie/coming-soon?count=${DEFAULT_COUNT}`
+  COMING_SOON: `http://api.douban.com/v2/movie/coming_soon?count=${DEFAULT_COUNT}`,
+  MOVIE: `https://movie.douban.com/subject/`
 }
 
 const bar = new ProgressBar(`进度 [:bar] ${chalk.yellowBright(':percent')}${chalk.yellow(' / :total%')} ${chalk.yellow(':info')} `, {
@@ -23,11 +25,93 @@ const bar = new ProgressBar(`进度 [:bar] ${chalk.yellowBright(':percent')}${ch
 })
 
 const show = async (ctx) => {
-  const jsonString = await rp(`${ctx.now ? URL.SHOW_THEATERS : URL.COMING_SOON }`)
-  if (ctx.now) {
-
-  } else if (ctx.soon) {
-    console.log('list soon')
+  bar.tick(10, { info: '获取豆瓣网接口数据...' })
+  const response = await rp(`${ctx.soon ? URL.COMING_SOON : URL.SHOW_THEATERS}`)
+  bar.tick(10, { info: '获取豆瓣网接口数据成功' })
+  bar.tick(5, { info: '筛选高评分电影...' })
+  const moviesInTheaterOriginal = JSON.parse(response).subjects.map(movie => {
+    const { genres, title, original_title, alt, id } = movie
+    return {
+      average_rating: movie.rating.average,
+      genres: genres.join(' / '),
+      title,
+      original_title,
+      alt,
+      id,
+      image: movie.images.medium
+    }
+  })
+  const moviesInTheater = moviesInTheaterOriginal.filter(movie => { return (Number(movie.average_rating) >= 6) })
+  bar.tick(5, { info: '筛选高评分电影成功' })
+  const htmlPromises = moviesInTheater.map(async movie => await rp(movie.alt))
+  bar.tick(10, { info: '获取每部高评分电影详细信息...' })
+  for (let index = 0; index < htmlPromises.length; index++) {
+    const htmlString = await htmlPromises[index]
+    let $ = cheerio.load(htmlString)
+    const actors = $('span.actor .attrs').text()
+    const directors = $('a[rel="v:directedBy"]').text()
+    const releaseDate = $('span[property="v:initialReleaseDate"]').text()
+    const runtime = $('span[property="v:runtime"]').text()
+    const buyLink = ctx.now ? decodeURIComponent($('a.ticket-btn').attr('href').split('url=')[1]) : ''
+    const summary = $('span[property="v:summary"]').text().split('\n').map(string => string.trim()).filter(string => string !== '')
+    const roleList = Array.from($('.celebrities-list .celebrity').map((index, celebrity) => {
+      return {
+        name: $(celebrity).find('span.name').text(),
+        role: $(celebrity).find('span.role').text()
+      }
+    }))
+    if (!ctx.soon) {
+      try {
+        const commentHtmlString = await rp(`${URL.MOVIE}${moviesInTheater[index].id}/comments`)
+        $ = cheerio.load(commentHtmlString)
+        const comments = Array.from($('.comment-item').map((index, comment) => {
+          return {
+            commentTime: $(comment).find('span.comment-time').text().replace(/^\s+|\s+$/g, ''),
+            commentUser: $(comment).find('span.comment-info a').text(),
+            commentDetail: $(comment).find('div.comment p').text().replace(/^\s+|\s+$/g, ''),
+            commentVote: $(comment).find('.comment-vote span.votes').text().replace(/^\s+|\s+$/g, '')
+          }
+        })).slice(0, 10)
+        Object.assign(moviesInTheater[index], { actors, directors, releaseDate, runtime, buyLink, summary, roleList, comments })
+      } catch (err) {
+        continue;
+      }
+    }
+    Object.assign(moviesInTheater[index], { actors, directors, releaseDate, runtime, buyLink, summary, roleList })
+    bar.tick(Math.ceil(40 / htmlPromises.length))
+  }
+  bar.tick(5, { info: '格式化内容...' })
+  const print = moviesInTheater.map(movie => {
+    return `
+![${movie.title}](${movie.image})
+### ${movie.title}           ${movie.average_rating}
+##### ${movie.original_title}
+- 导演： ${movie.directors !== '' ? movie.directors : '暂无信息'}
+- 主演： ${movie.actors !== '' ? movie.actors : '暂无信息'}
+- 类型： ${movie.genres}
+- 上映日期： ${movie.releaseDate !== '' ? movie.releaseDate : '暂无信息'}
+- 影片时长： ${movie.runtime !== '' ? movie.runtime : '暂无信息'}
+- 剧情简介：${movie.summary ? '\n' + movie.summary.join('\n') : '暂无信息'}
+- 演职员表：${movie.roleList ? '\n' + movie.roleList.filter(role => role.role !== '').map((role, index) => `  - **${role.name}**    ${role.role}\n\n`).join('') : '暂无信息'}
+${ctx.now ? `- 网友热评：${movie.comments !== undefined ? '\n' + movie.comments.map((comment, index) => `> **${index + 1}   ${comment.commentUser}**         ${comment.commentTime}        ${comment.commentVote}\n> ${comment.commentDetail}\n\n`).join('') : '暂无评论'}` : ''}
+${ctx.now ? `- 购票链接： [${movie.title}](${movie.buyLink})` : ''}
+      `
+    })
+  bar.tick(5, { info: '格式化内容成功，准备写入' })
+  if (ctx.parent.file) {
+    return fs.writeFile(ctx.parent.file, print.join('\n\n'), 'utf-8', (err) => {
+      if (err) throw err
+      else {
+        bar.tick(10, { info: '内容写入成功' })
+        console.log(`数据已经输出在 ${chalk.underline.bold.red(path.resolve(process.cwd(), ctx.parent.file))}`)
+      }
+    })
+  } else {
+    bar.tick(10, { info: '内容写入成功' })
+    print.forEach(item => {
+      console.log(item)
+      console.log('')
+    })
   }
 }
 
